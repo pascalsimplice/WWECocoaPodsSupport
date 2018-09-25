858a9af65ddad9bbf9924a00d58af6beb8c0f237
Pascal Simplice <pascal@ayoupa.com>
2018-09-25 21:10:48 +0100

    Re-added public header

---
+++ Source/PublicHeader.h
@@ -0,0 +1,13 @@
+//
+//  PublicHeader.h
+//  WWECocoaPodsSupport
+//
+//  Created by Pascal Simplice on 25/09/2018.
+//
+
+#ifndef PublicHeader_h
+#define PublicHeader_h
+
+#import EasyPeasy
+
+#endif /* PublicHeader_h */
