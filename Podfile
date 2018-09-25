4f15c9ed7f2d1ea13ea708ea6dc781326b80d26a
Pascal Simplice <pascal@ayoupa.com>
2018-09-25 13:02:14 +0100

    Re-created project

--- Podfile
+++ Podfile
@@ -1,11 +1,11 @@
 # Uncomment the next line to define a global platform for your project
-target 'WWECocoaPodsSupport-iOS' do
+target 'WWECocoaPodsSupport iOS' do
     platform :ios, '9.0'
     pod 'GoogleTagManager', '6.0'
     pod 'EasyPeasy'
 end
 
-target 'WWECocoaPodsSupport-tvOS' do
+target 'WWECocoaPodsSupport tvOS' do
     platform :tvos, '9.0'
     pod 'EasyPeasy'
 end
---
+++ Source/WWECocoaPodsSupport_tvOS.h
@@ -0,0 +1,18 @@
+//
+//  WWECocoaPodsSupport_tvOS.h
+//  WWECocoaPodsSupport tvOS
+//
+//  Created by Pascal Simplice on 25/09/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WWECocoaPodsSupport_tvOS.
+FOUNDATION_EXPORT double WWECocoaPodsSupport_VersionNumber;
+
+//! Project version string for WWECocoaPodsSupport_tvOS.
+FOUNDATION_EXPORT const unsigned char WWECocoaPodsSupport_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WWECocoaPodsSupport_tvOS/PublicHeader.h>
+
+
