88aae9b1ce1b5d0cdfc68b49e40af54418d1787d
Pascal Simplice <pascal@ayoupa.com>
2018-09-25 20:44:28 +0100

    Fixed module map issue

--- Source/WWECocoaPodsSupport.h
+++ Source/WWECocoaPodsSupport.h
@@ -1,19 +1,19 @@
 //
-//  WWECocoaPodsSupport_tvOS.h
-//  WWECocoaPodsSupport tvOS
+//  WWECocoaPodsSupport_iOS.h
+//  WWECocoaPodsSupport iOS
 //
 //  Created by Pascal Simplice on 25/09/2018.
 //
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for WWECocoaPodsSupport_tvOS.
+//! Project version number for WWECocoaPodsSupport_iOS.
 FOUNDATION_EXPORT double WWECocoaPodsSupport_VersionNumber;
 
-//! Project version string for WWECocoaPodsSupport_tvOS.
+//! Project version string for WWECocoaPodsSupport_iOS.
 FOUNDATION_EXPORT const unsigned char WWECocoaPodsSupport_VersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like
-#import <WWECocoaPodsSupport_tvOS/PublicHeader.h>
+#import <WWECocoaPodsSupport/PublicHeader.h>
 
 
